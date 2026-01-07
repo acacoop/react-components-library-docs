4c77b944982fad03412eb48a7420554bfc8da8b5
Máximo Gismondi <maximogismondi@gmail.com>
2026-01-07 19:47:27 +0000

    fix: update base path in Vite configuration to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/components-library-docs/",
+  base: "/react-components-library-docs/",
   server: {
     port: 3000,
   },
